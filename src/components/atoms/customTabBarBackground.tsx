fc30b819dd0de2952cf92ba1d7bda8c4cbeb9cef
Azrael Landres <azraellandres@gmail.com>
2025-05-05 19:39:23 +0200

    refactor: clean up header and tab bar background with StyleSheet and simplified layout

--- src/components/atoms/customTabBarBackground.tsx
+++ src/components/atoms/customTabBarBackground.tsx
@@ -1,58 +1,53 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function CustomTabBarBackground() {
   return (
-    <View
-      style={{
-        position: "absolute",
-        bottom: 0,
-        left: 0,
-        right: 0,
-        zIndex: 10,
-        backgroundColor: "#A2CC9C",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "white",
-          height: 50,
-          position: "absolute",
-          left: 0,
-          right: 0,
-          bottom: 105,
-          borderBottomLeftRadius: 50,
-          borderBottomRightRadius: 50,
-          zIndex: 3,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "#A2CC9C",
-          height: 20,
-          width: 20,
-          position: "absolute",
-          left: 0,
+    <View style={styles.container}>
+      {/** Gives border radius to create illusion of an inverted radius*/}
+      <View style={styles.inverseBorder} />
 
-          bottom: 130,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "#A2CC9C",
-          height: 20,
-          width: 20,
-          position: "absolute",
-
-          right: 0,
-          bottom: 130,
-        }}
-      />
-      <View
-        style={{
-          height: 130,
-          zIndex: 2,
-        }}
-      ></View>
+      {/** little patches to permit a stronger radius */}
+      {["left", "right"].map((side) => (
+        <View
+          key={side}
+          style={[styles.patch, side === "left" ? { left: 0 } : { right: 0 }]}
+        />
+      ))}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    zIndex: 10,
+    height: 130,
+    backgroundColor: "#A2CC9C",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  navContainer: {
+    zIndex: 2,
+    height: 130,
+  },
+  inverseBorder: {
+    backgroundColor: "white",
+    height: 50,
+    position: "absolute",
+    left: 0,
+    right: 0,
+    bottom: 105,
+    borderBottomLeftRadius: 50,
+    borderBottomRightRadius: 50,
+    zIndex: 3,
+  },
+  patch: {
+    backgroundColor: "#A2CC9C",
+    height: 20,
+    width: 20,
+    position: "absolute",
+
+    bottom: 130,
+  },
+});
--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -1,18 +1,9 @@
 import LogoIcon from "@atoms/LogoIcon";
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 const Header = () => (
-  <View
-    style={{
-      zIndex: 10,
-      height: 130,
-      backgroundColor: "#b3aee4",
-      position: "absolute",
-      top: 0,
-      left: 0,
-      right: 0,
-    }}
-  >
+  <View style={styles.container}>
+    {/** View containg logo */}
     <View
       style={{
         zIndex: 2,
@@ -25,42 +16,57 @@ const Header = () => (
     >
       <LogoIcon />
     </View>
-    <View
-      style={{
-        backgroundColor: "white",
-        height: 50,
-        position: "absolute",
-        left: 0,
-        right: 0,
-        top: 105,
-        borderTopLeftRadius: 50,
-        borderTopRightRadius: 50,
-        zIndex: 3,
-      }}
-    />
-    <View
-      style={{
-        backgroundColor: "#b3aee4",
-        height: 20,
-        width: 20,
-        position: "absolute",
-        left: 0,
+    {/** Gives border radius to create illusion of an inverted radius*/}
 
-        top: 130,
-      }}
-    />
-    <View
-      style={{
-        backgroundColor: "#b3aee4",
-        height: 20,
-        width: 20,
-        position: "absolute",
+    <View style={styles.inverseBorder} />
 
-        right: 0,
-        top: 130,
-      }}
-    />
+    {/** little patches to permit a stronger radius */}
+    {["left", "right"].map((side) => (
+      <View
+        key={side}
+        style={[styles.patch, side === "left" ? { left: 0 } : { right: 0 }]}
+      />
+    ))}
   </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    zIndex: 10,
+    height: 130,
+    backgroundColor: "#b3aee4",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+  },
+  logoContainer: {
+    zIndex: 2,
+    height: 130,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingBottom: 20,
+  },
+  inverseBorder: {
+    backgroundColor: "white",
+    height: 50,
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 105,
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+    zIndex: 3,
+  },
+  patch: {
+    backgroundColor: "#b3aee4",
+    height: 20,
+    width: 20,
+    position: "absolute",
+
+    top: 130,
+  },
+});
 
 export default Header;
