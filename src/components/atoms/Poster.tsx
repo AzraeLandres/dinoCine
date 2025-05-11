1cae6ef27b0710bc986c7dd1ed61403abc29b3f3
Azrael Landres <azraellandres@gmail.com>
2025-05-11 18:08:51 +0200

    fix : improve poster layout

--- src/components/atoms/Poster.tsx
+++ src/components/atoms/Poster.tsx
@@ -3,7 +3,7 @@ import { Image } from "react-native";
 export default function Poster({ path }: { path: string }) {
   return (
     <Image
-      className="w-40 h-60 rounded-lg"
+      className="w-36 h-52 rounded-xl"
       source={{ uri: `https://image.tmdb.org/t/p/w500${path}` }}
     />
   );
